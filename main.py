19f5fcca4d3fd2305e60d0f012323c8fcd383149
unkownvenom <75563627+unkownvenom@users.noreply.github.com>
2020-12-06 15:03:55 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+# write commands to install modules/lib and run your project, one in each line.
+# python3, pip3 and poetry commands are available along with other basic linux commands.
+# command to run
+poetry run python3 main.py
